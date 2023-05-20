2f379c140ceed71aa301dc400ea6582636641623
Vedat Kara <vedatkara@mail.com>
2023-05-21 01:03:13 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -4,43 +4,48 @@ import java.util.Scanner;
 public class Main {
 
     public static int Greedy(int year, int promo, int coach, int[] demands, int[] salaries) {
-        int minCost = 0, diff, remaining = 0, nextDiff = 0, cost = 0, cost_1, cost_2 = 0;
-        boolean is_there_remaining = false;
+        int minCost = 0, diff, remaining = 0, nextDiff, cost, cost_1, cost_2;
+        boolean is_there_remaining = false; // flag to consider remaining players from previous year
 
         for (int i = 1; i <= year; i++) {
-            diff = demands[i] - promo;
+            diff = demands[i] - promo;  // difference between current year's demand and promotion number.
 
+            /* If there are remaining players from previous year subtract them from difference. */
             if (is_there_remaining) {
                 diff -= remaining;
                 is_there_remaining = false;
+
+            /* else reset number of remaining players */
             } else
                 remaining = 0;
 
+            // If there is difference just pay for the coach
             if (diff > 0)
                 minCost += coach * diff;
 
+            // Else decide whether keeping the players or release them considering next year's demand and minimum cost
              else if (diff < 0) {
                     nextDiff = demands[i + 1] - promo;
 
                     if (nextDiff > 0) {
                         cost_2 = 0;
                         diff *= -1;
-                        cost = coach * nextDiff;
+                        cost = coach * nextDiff; // Cost for no remaining players
 
                         for (int j = 1; j <= nextDiff; j++) {
-                            cost_1 = salaries[j] + ((nextDiff - j) * coach) + ((j - diff) * coach);
+                            cost_1 = salaries[j] + ((nextDiff - j) * coach) + ((j - diff) * coach); // cost with remaining players
 
-                            if(cost_1 < cost) {
+                            if(cost_1 < cost) { // If keeping the players is more efficient enter
                                 if(j > diff)
                                     cost_2 = (j - diff) * coach + salaries[j];
                                 else
                                     cost_2 = salaries[j];
 
-                                is_there_remaining = true;
+                                is_there_remaining = true; // Flag to know there are remaining players
                                 remaining = j;
                             }
                         }
-                        minCost += cost_2;
+                        minCost += cost_2; // Update minimum cost
                     }
 
             }
