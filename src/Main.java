383eeee8dc4fd466150f65814f5cc3e6bcfc68ad
Vedat Kara <vedatkara@mail.com>
2023-05-20 18:30:45 +0300

    End

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,62 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int DP(int n, int p, int c, int[] yearlyPlayerDemand, int[] playerSalary){
-        int[][] dp = new int[n + 1][p + 1];
+    public static int Greedy(int year, int promo, int coach, int[] demands, int[] salaries) {
+        int minCost = 0, diff, remaining = 0, nextDiff = 0, cost = 0, cost_1, cost_2 = 0;
+        boolean is_there_remaining = false;
 
-        for (int i = 1; i <= n; i++) {
-            int demand = yearlyPlayerDemand[i - 1];
-            int salary = playerSalary[i - 1];
+        for (int i = 1; i <= year; i++) {
+            diff = demands[i] - promo;
 
-            for (int j = 0; j <= p; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
+            if (is_there_remaining) {
+                diff -= remaining;
+                is_there_remaining = false;
+            } else
+                remaining = 0;
 
-                if (j >= demand) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - demand]);
-                }
+            if (diff > 0)
+                minCost += coach * diff;
 
-                if (j < p) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + salary);
-                }
+             else if (diff < 0) {
+                    nextDiff = demands[i + 1] - promo;
+
+                    if (nextDiff > 0) {
+                        cost_2 = 0;
+                        diff *= -1;
+                        cost = coach * nextDiff;
+
+                        for (int j = 1; j <= nextDiff; j++) {
+                            cost_1 = salaries[j] + ((nextDiff - j) * coach) + ((j - diff) * coach);
+
+                            if(cost_1 < cost) {
+                                if(j > diff)
+                                    cost_2 = (j - diff) * coach + salaries[j];
+                                else
+                                    cost_2 = salaries[j];
+
+                                is_there_remaining = true;
+                                remaining = j;
+                            }
+                        }
+                        minCost += cost_2;
+                    }
+
             }
         }
-        int minTotalCost = dp[n][p];
-        return minTotalCost;
+        return minCost;
     }
 
+
     public static void main(String[] args) throws FileNotFoundException {
-        int n = 3, p= 5, c= 5, index = 1;
+        int n = 50, p = 7, c = 10, index = 1;
         String line;
-        String str_values[];
+        String[] str_values;
 
         int[] demands = new int[100];
         File file = new File("yearly_player_demand.txt");
         Scanner scan = new Scanner(file);
         scan.nextLine();
-        while(scan.hasNext()){
+        while (scan.hasNext()) {
             line = scan.nextLine();
             str_values = line.split("\t");
             demands[index] = Integer.parseInt(str_values[1]);
@@ -44,17 +67,19 @@ public class Main {
         scan.close();
 
         index = 1;
-        int[] salaries = new int[500];
+        int[] salaries = new int[311];
+        salaries[0] = 0;
         file = new File("players_salary.txt");
-        scan = new Scanner(file);
-        scan.nextLine();
-        while(scan.hasNext()){
-            line = scan.nextLine();
+        Scanner scn = new Scanner(file);
+        scn.nextLine();
+        while (scn.hasNext()) {
+            line = scn.nextLine();
             str_values = line.split("\t");
             salaries[index] = Integer.parseInt(str_values[1]);
-            System.out.println(salaries[index]);
             index++;
         }
+
+        System.out.println("Greedy Results: " + Greedy(n, p, c, demands, salaries));
 
 
     }
